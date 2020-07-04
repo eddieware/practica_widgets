f5dde10a3d2c52e2c8bf07b0482ab2796ddbe5d0
eddieware <47250727+eddieware@users.noreply.github.com>
2020-07-04 06:08:32 -0500

    pt1

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,82 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          title: Text("Exploring Widgets"),
         ),
-        body: Center(
-          child: Container(
-            //color: Colors.green,
-            child: myWidget(),
-          ),
-        ),
+        body: myLayoutWidget(),
       ),
     );
   }
 }
 
-// Widget myWidget() {
-//   return Center(
-//     child: Container(
-
-//       color: Colors.red,
-//       child: Text('Hola mundo'),
-//     ),
-//   );
-// }
-
-// Widget myWidget() {
-//   return Column(
-//     children: <Widget>[
-//       Padding(
-//         padding: EdgeInsets.fromLTRB(20.0, 10.0, 20, 40),
-//         child: Text('data'),
-//       ),
-//       Padding(
-//         padding: EdgeInsets.fromLTRB(300.0, 10.0, 0, 4),
-//         child: Text('Hola'),
-//       )
-//     ],
-//   );
-// }
-
-Widget myWidget() {
-  return Column(
-    children: <Widget>[
-      RaisedButton(
-        onPressed: null,
-      )
-    ],
+Widget myLayoutWidget() {
+  return Container(
+    margin: EdgeInsets.all(16.0),
+    padding: EdgeInsets.all(16.0),
+    decoration: BoxDecoration(
+      color: Colors.purple[900],
+      border: Border.all(),
+      borderRadius: BorderRadius.all(Radius.circular(3.0)),
+    ),
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Row(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(right: 8.0),
+              child: Icon(
+                Icons.favorite,
+                color: Colors.green,
+              ),
+            ),
+            Text(
+              'BEAMS',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: 16.0,
+            horizontal: 0,
+          ),
+          child: Text(
+            'Send programmable push notifications to iOS and Android devices with delivery and open rate tracking built in.',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        Row(
+          children: [
+            Text(
+              'EXPLORE BEAMS',
+              style: TextStyle(
+                color: Colors.green,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 8.0),
+              child: Icon(
+                Icons.arrow_forward,
+                color: Colors.green,
+              ),
+            ),
+          ],
+        ),
+      ],
+    ),
   );
 }
